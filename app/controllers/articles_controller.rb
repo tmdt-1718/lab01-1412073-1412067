0365ae536ee329895009ab92dead0e1583e4fe4c
npcuong308 <npcuong.011308@gmail.com>
2017-09-25 23:03:00 +0700

    Fix sql

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,54 @@
 class ArticlesController < ApplicationController
+#An controller includes: index, new, edit, create, update, show and destroy action
+#Controller takes request, gets data form users or database
+#to view data or show things to user, we use app/views/articles/<>.html.erb
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+	
+	def edit
+		@article = Article.find(params[:id])	
+	end
+	
+	def create
+		@article = Article.new(article_params)
+		
+		if @article.save
+			# show the submitted article
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	
+	def update 
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			# show the submitted article
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+	
+	private
+		def	article_params
+			params.require(:article).permit(:title, :text)
+		end
 end
