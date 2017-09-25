bf713885c50e53d60d2fe1f04c8f304987b33589
npcuong308 <npcuong.011308@gmail.com>
2017-09-25 22:06:06 +0700

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   README.md 	modified:   app/controllers/articles_controller.rb 	modified:   app/controllers/comments_controller.rb 	modified:   app/models/article.rb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/welcome/index.html.erb 	modified:   config/database.yml 	modified:   config/routes.rb 	modified:   config/secrets.yml 	modified:   db/schema.rb 	modified:   test/fixtures/comments.yml

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920142232) do
+ActiveRecord::Schema.define(version: 20170925145824) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "content"
-    t.integer "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
