78af78bdd6a0b9abb02861f42a441795f1d6a12b
npcuong308 <npcuong.011308@gmail.com>
2017-09-30 13:38:45 +0700

    Add navigation bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925150024) do
+ActiveRecord::Schema.define(version: 20170925170111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170925150024) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
-    t.text "body"
+    t.text "content"
     t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
