2271d7a5c2161b03ff6c094484de39bfd46fe9bd
npcuong308 <npcuong.011308@gmail.com>
2017-10-09 19:53:45 +0700

    Almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-(1..2).each do |user_num|
+
+image_name = [ "cauvang.jpg", "guibff.jpg", "gaben.jpg", "eroi.jpg"]
+
+(0..1).each do |user_num|
 	user = User.create!(name: Faker::Name.name, email: 'cuongkb3g', password: 'cuongkb3g')
 
 	album = user.albums.create!(creator: 'ktdanh', view: 0, coverphoto: 'http://genknews.genkcdn.vn/k:2016/img20160211125859390/dota-2-morphling--su-troi-day-cua-thuy-than-tren-dau-truong-chuyen-nghiep.jpg')
@@ -15,10 +18,11 @@ User.destroy_all
 	album.photos.create!(creator: 'npcuong', view: 0, source: 'https://s-media-cache-ak0.pinimg.com/originals/48/78/11/48781195b16acef9e886e0e93b5b7d8e.jpg')
 	album.photos.create!(creator: 'npcuong', view: 0, source: 'https://sd.keepcalm-o-matic.co.uk/i-w600/setu-picks-carry-ez-25-mmr.jpg')
 	album.photos.create!(creator: 'npcuong', view: 0, source: 'https://i.ytimg.com/vi/rcElmF0kyK8/hqdefault.jpg')
-	(1..2).each do |article_num|
-		article = user.articles.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph(10))
+	
+	(0..1).each do |article_num|
+		article = user.articles.create!(creator: Faker::Name.name ,coverphoto: image_name[2*user_num + article_num] ,view: 0, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph(10))
 
-		(1..2).each do |comment_count|
+		(0..1).each do |comment_count|
 			article.comments.create!(body: Faker::Lorem.paragraph, user_id: user.id)
 		end
 	end
